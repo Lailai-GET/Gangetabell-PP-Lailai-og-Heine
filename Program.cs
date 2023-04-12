2be9f9fc904a05501955e28f0f2491c7fba656d6
Nikolaipe <nikolaipe@getacademy.no>
2023-04-12 11:30:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Gangetabell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Bredden på skjermen er{Console.WindowWidth}");
+                Console.WriteLine("Skriv et tall");
+                var input = Console.ReadLine();
+                var isNumber = int.TryParse(input, out int number);
+
+                if (!isNumber) continue;
+                int width = number * 7;
+                Console.WindowWidth = width;
+                var numberList = new Row(number, width);
+
+                numberList.GenerateMatrix();
+                Console.WriteLine("Trykk enter for nytt tall");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Row.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace Gangetabell
+{
+    internal class Row
+    {
+        private int[] _numberList;
+        private int _width;
+        private int Padding => _width / _numberList.Length;
+
+        public Row(int userInput, int width)
+        {
+            _numberList = new int[userInput];
+            _width = width;
+        }
+
+        public void GenerateMatrix()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "TextFile.txt");
+            List<string> listOfLines = new List<string>();
+            for (int j = 1; j < _numberList.Length + 1; j++)
+            {
+                string numbersInLine = string.Empty;
+                SetNumbers(j);
+                foreach (var number in _numberList)
+                {
+                    string paddedString = $"{number}, ";
+                    Console.Write(paddedString.PadLeft(Padding, ' '));
+                    numbersInLine += $"{paddedString}";
+
+                }
+                listOfLines.Add(numbersInLine);
+
+            }
+            File.WriteAllLines(path, listOfLines);
+        }
+
+        private void SetNumbers(int jndex)
+        {
+            for (int i = 1; i < _numberList.Length + 1; i++)
+            {
+                _numberList[i - 1] = jndex * i;
+            }
+        }
+    }
+}
